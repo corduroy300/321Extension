5705f688f97197d5b3281517ac8eb853b3da04db
unknown <vpham8@masonlive.gmu.edu>
2021-03-19 00:38:15 -0400

    Background.js -- Started work on tabTack function Manifest.json  -- Added additional permission for chrome api Popup.html -- Added timer

--- background.js
+++ background.js
@@ -1,6 +1,43 @@
-chrome.tabs.onActivated.addListener(tab =>{
+
+
+/*chrome.tabs.onActivated.addListener(tab =>{
     chrome.tabs.get(tab.tabId, tab_info =>{
         console.log(tab_info.url)
     });
+});*/
+console.log("hello world");
+
+chrome.tabs.onActiveChanged.addListener(function (windowId) {
+    if (windowId == chrome.windows.WINDOW_ID_NONE){
+        tabTrack(false);
+    }
+    else{
+        tabTrack(true);
+    }
 });
-console.log("hello world")
+
+function tabTrack(isWindowActive){
+
+    chrome.tabs.query({'active': true}, function(tabs){
+
+        //console.log(tabs);
+
+        let url = tabs[0].url;
+        let title = tabs[0].title;
+        let currentTab = tabs[0];
+        let hostName = url;
+        try{
+            let urlObject = new URL(url);
+            hostName = urlObject.hostname;
+        } catch (e){
+            console.log("Error in getting hostname from url");
+        }
+        console.log(hostName);
+
+
+
+
+    });
+}
+
+
