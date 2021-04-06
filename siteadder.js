f32bc56c736c3092071b76bcb2329abc270f99ab
Ujwal Sharma <ujwalsharma19139@gmail.com>
2021-04-06 19:33:26 -0400

    adding to chrome storage duplicate

--- siteadder.js
+++ siteadder.js
@@ -1,13 +1,14 @@
-const unproductiveTabsKey = "unproductiveTabs";
+//const unproductiveTabsKey = "unproductiveTabs";
 var unproductiveTabs;
 
+//sets the unproductiveTabs object to the value saved in chrome storage
 chrome.storage.sync.get(unproductiveTabsKey, function (result) {
     let listOfUnproductiveTabs = result[unproductiveTabsKey];
-
     //need to parse the string in storage into JSON objects
     if (listOfUnproductiveTabs != null) {
+        //alert("This happened");
         unproductiveTabs = JSON.parse(listOfUnproductiveTabs);
-    }
+    } 
 });
 
 document.getElementById("hostname").addEventListener('keypress', function (e) {
@@ -16,28 +17,37 @@ document.getElementById("hostname").addEventListener('keypress', function (e) {
     }
 });
 
+/* Adds user specified sites into unproductive list
+* Checks to make sure that this site  isn't already added
+*/
 function addToStorage() {
+
     var siteName = document.getElementById("hostname").value;
 
-    alert("here site name is: " + siteName);
     var infoOfSite = {
-        url: siteName,
-        timeSpent: 0,
-        lastTimeVisited: 0,
+        "url": siteName,
+        "timeSpent": 0,
+        "lastTimeVisited": 0,
     };
 
-    //alert(unproductiveTabsList[siteName]);
-
-    if(typeof unproductiveTabs[siteName] === 'undefined'){
+    if(unproductiveTabs == null){
+        unproductiveTabs =  {};
         unproductiveTabs[siteName] = infoOfSite;
-        alert("added accessing url from unproductiveTabList Object: " + unproductiveTabs[siteName].url);
+        alert("added because list is empty " + unproductiveTabs[siteName].url);
+    } else if(typeof unproductiveTabs[siteName] === 'undefined'){
+        unproductiveTabs[siteName] = infoOfSite;
+        alert("added because this is new url " + unproductiveTabs[siteName].url);
+    } else {
+        alert("site was not added because it is duplicate");
     }
 
+    //After setting the new site name, we need to update this object in chrome storage so that the values are updated across all files
     const tabTimesObjectString = JSON.stringify(unproductiveTabs);
     let newTabTimesObject = {};
-    newTabTimesObject[unproductiveTabsKey] = tabTimesObjectString;
+    newTabTimesObject["unproductiveTabs"] = tabTimesObjectString;
     chrome.storage.sync.set(newTabTimesObject, function () {
-
+        chrome.storage.sync.get("unproductiveTabs", function (result) {
+            alert("current unproductiveTabsList is: " + result["unproductiveTabs"])});
     });
 
 }
